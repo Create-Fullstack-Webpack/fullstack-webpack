571e5e80f90e57c2fd9382d55cfd5753b353e4a3
kiso <dotkiso@gmail.com>
2020-08-19 10:08:33 -0400

    generateBackend

--- scripts/generateBackend.js
+++ scripts/generateBackend.js
@@ -1,4 +1,21 @@
-const path = require('path');
+// const path = require('path');
+const fs = require('fs');
+const server = require('./data/backend/server.js');
 
-const dir = process.cwd() + "/src/client";
-console.log(dir);
+function generateBackend( framework ) {
+  // framework = 'express';
+  // if (framework === 'express') {
+    const dir = process.cwd() + '/server/';
+    console.log(dir);
+  
+    fs.mkdir(dir, (err) => {
+      if (err)
+        console.log(err);
+      fs.writeFileSync(path.resolve(dir, 'server.js'), server);
+      fs.mkdir(dir + 'controller', (err) => {
+        if (err)
+          console.log(err);
+      });
+    });
+  // }
+}
